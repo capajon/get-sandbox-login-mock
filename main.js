e53fc62e4ae2cbdc425de5b3ce007332e2d5f4d0
Jon Capa <jon.capa@D2L.com>
2015-08-21 11:43:28 -0400

    Added basic tool access mocks

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var generateLoginData = require('login-data.js').generate,
-    loginConsts = require('login-data.js').consts;
+    loginConsts = require('login-data.js').consts,
+    generateToolAccessData = require('tool-access-data.js').generate,
+    toolAccessConsts = require('tool-access-data.js').consts;
 
-var settings = {
+var loginSettings = {
     'daily': {
         'startOverwrite': null, //options for date range overrides: null (for no override) or 'YYYY/MM/DD' format or timestamp    
         'endOverwrite': null,
@@ -31,7 +33,25 @@ Sandbox.define('/d2l/api/adp/unstable/events/userinteractionevent/login/count','
         throw('Unknown granularity of ' + granularity);
     }
     
-    var result = generateLoginData(granularity, req.query.start, req.query.end, settings);
+    var result = generateLoginData(granularity, req.query.start, req.query.end, loginSettings);
+    
+    res.type('application/json');
+    res.status(200);
+    res.json(result);
+})
+
+// tool-access is very 'dumb' for now, only returning one test set - needs similar features as logins to be added
+Sandbox.define('/d2l/api/adp/unstable/events/userinteractionevent/view/count','GET', function(req, res) {
+    var granularity = req.query.granularity;
+    if (
+        granularity !== toolAccessConsts.SEVEN_DAYS && 
+        granularity !== toolAccessConsts.THIRTY_DAYS &&
+        granularity !== toolAccessConsts.TWELVE_MONTHS
+    ) {
+        throw('Unknown granularity of ' + granularity);
+    }
+    
+    var result = generateToolAccessData(granularity);
     
     res.type('application/json');
     res.status(200);
