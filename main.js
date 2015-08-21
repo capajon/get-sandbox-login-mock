667aa9456b6edf290b70ef03af1b796429d5bb39
Jon Capa <jon.capa@D2L.com>
2015-08-21 13:11:41 -0400

    Removing tool access and moving to separate repo

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var generateLoginData = require('login-data.js').generate,
-    loginConsts = require('login-data.js').consts,
-    generateToolAccessData = require('tool-access-data.js').generate,
-    toolAccessConsts = require('tool-access-data.js').consts;
+    loginConsts = require('login-data.js').consts;
 
 var loginSettings = {
     'daily': {
@@ -34,24 +32,6 @@ Sandbox.define('/d2l/api/adp/unstable/events/userinteractionevent/login/count','
     }
     
     var result = generateLoginData(granularity, req.query.start, req.query.end, loginSettings);
-    
-    res.type('application/json');
-    res.status(200);
-    res.json(result);
-})
-
-// tool-access is very 'dumb' for now, only returning one test set - needs similar features as logins to be added
-Sandbox.define('/d2l/api/adp/unstable/events/userinteractionevent/view/count','GET', function(req, res) {
-    var granularity = req.query.granularity;
-    if (
-        granularity !== toolAccessConsts.SEVEN_DAYS && 
-        granularity !== toolAccessConsts.THIRTY_DAYS &&
-        granularity !== toolAccessConsts.TWELVE_MONTHS
-    ) {
-        throw('Unknown granularity of ' + granularity);
-    }
-    
-    var result = generateToolAccessData(granularity);
     
     res.type('application/json');
     res.status(200);
