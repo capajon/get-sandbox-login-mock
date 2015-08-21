dec4a5b0406cc3aee3380ab5bfe9df3da6cc6def
Jon Capa <jon.capa@D2L.com>
2015-08-21 11:26:01 -0400

    Initial version

---
+++ login-data.js
@@ -0,0 +1,95 @@
+var consts =  {
+    'DAILY_CODE': 'D',
+    'MONTHLY_CODE': 'M',
+    'MS_PER_DAY': 86400000
+};
+
+var generate = function generate(granularity, startTs, endTs, settings){
+    startTs = trySetStartOverride(startTs, granularity, settings);
+    endTs = trySetEndOverride(endTs, granularity, settings);
+    
+    var localStartDate = new Date(+startTs), // + forces string to int
+        localEndDate = new Date(+endTs),
+        localOffset = localStartDate.getTimezoneOffset() * 60000,
+        result = [];
+        
+    var startDate = (granularity === consts.MONTHLY_CODE) ? 1 : localStartDate.getDate(),
+        startUtc = new Date(localStartDate.getFullYear(), localStartDate.getMonth(), startDate).getTime() - localOffset,
+        endDate = (granularity === consts.MONTHLY_CODE) ? 1 : localEndDate.getDate(),
+        endUtc = new Date(localEndDate.getFullYear(), localEndDate.getMonth(), endDate).getTime() - localOffset,
+        curUtcTs = startUtc;
+    
+    while (curUtcTs <= endUtc) {
+        result.push({
+           "Time": curUtcTs,
+           "Count": getRandomCount(curUtcTs, granularity, localOffset, settings)
+        });
+        curUtcTs = getNextTs(curUtcTs, granularity, localOffset); 
+    }
+
+    return result;
+};
+
+var getNextTs = function getNextTs(curUtcTs, granularity, localOffset) {
+    if(granularity === consts.DAILY_CODE){
+      return curUtcTs + consts.MS_PER_DAY;  
+    } else { //month
+        var curDate = new Date(curUtcTs);
+        return new Date(curDate.getFullYear(), curDate.getMonth() + 2, 1).getTime() - localOffset;
+    }
+};
+
+var getRandomCount = function getRandomCount(timestamp, granularity, localOffset, settings){
+    var randomCountMax = (granularity === consts.MONTHLY_CODE) ? settings.monthly.randomCountMax : settings.daily.randomCountMax,
+        randomCountMin = (granularity === consts.MONTHLY_CODE) ? settings.monthly.randomCountMin : settings.daily.randomCountMin;
+    
+    var randomCount = Math.floor(Math.random() * (randomCountMax - randomCountMin + 1)) + randomCountMin;
+    
+    return trySetZeroCountDateRanges(randomCount, timestamp, granularity, localOffset, settings);
+};
+
+var trySetStartOverride = function trySetStartOverride(startTs, granularity, settings) {
+    return trySetOverride(
+        startTs,
+        (granularity === consts.MONTHLY_CODE) ? settings.monthly.startOverwrite : settings.daily.startOverwrite
+    );
+};
+
+var trySetZeroCountDateRanges = function trySetZeroCountDateRanges(randomCount, timestamp, granularity, localOffset, settings){
+    var zeroCountDateRanges = (granularity === consts.MONTHLY_CODE) ? 
+        settings.monthly.zeroCountDateRanges : 
+        settings.daily.zeroCountDateRanges,
+        result = randomCount;
+        
+    zeroCountDateRanges.forEach(function(dateRange){
+        if(typeof dateRange !== 'undefined' &&
+            dateRange !== null  && 
+            timestamp >= (new Date(dateRange.start).getTime() - localOffset) &&
+            timestamp <= (new Date(dateRange.end).getTime() - localOffset)
+        ) {
+            result = 0;
+        }
+    });
+    
+    return result;
+};
+
+var trySetEndOverride = function trySetEndOverride(endTs, granularity, settings) {
+    return trySetOverride(
+        endTs,
+        (granularity === consts.MONTHLY_CODE) ? settings.monthly.endOverwrite : settings.daily.endOverwrite
+    );
+};
+
+var trySetOverride = function trySetOverride(timestamp, override) {
+    if (typeof override != 'undefined' && override !== null) {
+        return new Date(override).getTime();
+    } else {
+        return timestamp;
+    }
+}
+
+module.exports = {
+    'generate': generate,
+    'consts': consts
+};
---
+++ main.js
@@ -0,0 +1,39 @@
+var generateLoginData = require('login-data.js').generate,
+    loginConsts = require('login-data.js').consts;
+
+var settings = {
+    'daily': {
+        'startOverwrite': null, //options for date range overrides: null (for no override) or 'YYYY/MM/DD' format or timestamp    
+        'endOverwrite': null,
+        'randomCountMax': 10000,
+        'randomCountMin': 2000,
+        // empty (for no gaps) or 'YYYY/MM/DD' format or timestamp pairs - e.g. { 'start': '1999/04/01', 'end': '2000/05/01' }
+        'zeroCountDateRanges': [
+            { 'start': '2015/02/02', 'end': '2015/02/25' },
+            { 'start': '2015/08/05', 'end': '2015/09/20' }
+        ]
+    },
+    'monthly': {
+        'startOverwrite': null, // null (for no override) or 'YYYY/MM/DD' format or timestamp    
+        'endOverwrite': null,
+        'randomCountMax': 300000,
+        'randomCountMin': 60000,
+        // empty (for no gaps) or 'YYYY/MM/DD' format or timestamp pairs - e.g. { 'start': '1999/04/01', 'end': '2000/05/01' }
+        'zeroCountDateRanges': [ 
+            { 'start': '2010/01/01', 'end': '2014/01/1' }
+        ]
+    }
+};
+
+Sandbox.define('/d2l/api/adp/unstable/events/userinteractionevent/login/count','GET', function(req, res) {
+    var granularity = req.query.granularity;
+    if (granularity !== loginConsts.MONTHLY_CODE && granularity !== loginConsts.DAILY_CODE) {
+        throw('Unknown granularity of ' + granularity);
+    }
+    
+    var result = generateLoginData(granularity, req.query.start, req.query.end, settings);
+    
+    res.type('application/json');
+    res.status(200);
+    res.json(result);
+})
